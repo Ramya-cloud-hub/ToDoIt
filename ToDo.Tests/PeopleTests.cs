d9ccca3ea95cb3a2befd25b578f5954956894b78
Ramya-cloud-hub <ramya118gowda@gmail.com>
2021-06-29 11:19:42 +0200

    People Class

---
+++ ToDo.Tests/PeopleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using ToDo.Data;
+
+namespace ToDo.Tests
+{
+    public class PeopleTests
+    {
+        [Fact]
+        public void Check_Size_People_Array_Test()
+        {
+            //Assert
+            
+        }
+    }
+}
