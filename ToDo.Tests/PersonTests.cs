446da2f474e3e6ca7bd0e9b58937c44783a09b90
Ramya-cloud-hub <ramya118gowda@gmail.com>
2021-07-02 09:21:10 +0200

    Final Changes PersonTests

--- ToDo.Tests/PersonTests.cs
+++ ToDo.Tests/PersonTests.cs
@@ -28,34 +28,75 @@ namespace ToDo.Tests
             Assert.NotEqual(firstName, p1.LastName);
             Assert.NotEqual(lastName, p1.FirstName);
         } 
-        [Fact]
-        public void Test_People_PropertyType()
-        {              //Arrange
-        int id = 123;
-        string firstName = "Ramya";
-        string lastName = " Srinivs";
 
-        //Act
-        Person p1 = new Person(firstName, lastName, id);
-
-            //Assert
-            Assert.IsType<string>(p1.FirstName);
-            Assert.IsType<string>(p1.LastName);
-            Assert.IsType<int>(p1.PersonId);
-        }
-        [Fact]
-        public void Test_Bad_People()
+       [Fact]
+        public void Test_Constructor_Bad_Person_Null()
         {
             //Arrange
             int id = 1;
-            int id1 = 2;
             string firstName = null;
             string lastName = null;
+
+            Assert.Throws<ArgumentException>(() => new Person(firstName, lastName, id));            
+        }
+        [Fact]
+        public void Test_Constructor_Bad_Person_WhiteSpace()
+        {
+            //Arrange
+            int id1 = 2;
             string firstName1 = " ";
             string lastName1 = " ";
 
-            Assert.Throws<ArgumentException>(() => new Person(firstName, lastName, id));
             Assert.Throws<ArgumentException>(() => new Person(firstName1, lastName1, id1));
+        }
+        [Fact]
+        public void Test_Constructor_Bad_Person_Empty()
+        {
+            //Arrange
+            int id1 = 2;
+            string firstName1 = "";
+            string lastName1 = "";
+
+            Assert.Throws<ArgumentException>(() => new Person(firstName1, lastName1, id1));
+        }
+        [Fact]
+        public void Test_Bad_Person_Property_Null()
+        {
+            int id = 1;
+           string firstName = "Ramya";
+            string lastName = "Gowda";
+            Person p = new Person(firstName, lastName, id);
+            string firstName1 = null;
+            string secondName1 = null;
+
+           Assert.Throws<ArgumentException>(() => p.FirstName=firstName1);
+           Assert.Throws<ArgumentException>(() => p.LastName =secondName1);       
+        }
+        [Fact]
+        public void Test_Bad_Person_Property_WhiteSpace()
+        {
+            int id = 1;
+            string firstName = "Ramya";
+            string lastName = "Gowda";
+            Person p = new Person(firstName, lastName, id);
+            string firstName1 = " ";
+            string secondName1 = " ";
+
+            Assert.Throws<ArgumentException>(() => p.FirstName = firstName1);
+            Assert.Throws<ArgumentException>(() => p.LastName = secondName1);
+        }
+        [Fact]
+        public void Test_Bad_Person_Property_Empty()
+        {
+            int id = 1;
+            string firstName = "Ramya";
+            string lastName = "Gowda";
+            Person p = new Person(firstName, lastName, id);
+            string firstName1 = "";
+            string secondName1 = "";
+
+            Assert.Throws<ArgumentException>(() => p.FirstName = firstName1);
+            Assert.Throws<ArgumentException>(() => p.LastName = secondName1);
         }
     }
 }
